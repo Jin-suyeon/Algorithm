3151473d8bd5825f4c6fc06bcf5b02543b875b55
Jin-suyeon <wlstndus99@gmail.com>
2021-09-15 22:24:32 +0900

    예산

---
+++ Programmers/Level_1/예산.js
@@ -0,0 +1,17 @@
+function solution(d, budget) {
+  let count = 0;
+  let sum = 0;
+
+  d.sort((a, b) => a - b);
+
+  for (let i = 0; i < d.length; i++) {
+    if (sum + d[i] <= budget) {
+      sum = sum + d[i];
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(solution([1, 3, 2, 5, 4], 9));
+console.log(solution([2, 2, 3, 3], 10));
