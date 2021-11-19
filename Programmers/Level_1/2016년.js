638ada229c588fd1b7103df75eb0429f953b531a
Jin-suyeon <wlstndus99@gmail.com>
2021-11-19 13:54:03 +0900

    2016년

---
+++ Programmers/Level_1/2016년.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  return days[new Date(2016, a - 1, b).getDay()];
+}
+
+
+console.log(solution(5, 24))
