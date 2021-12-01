7d5bc9bbb62ff66bfa17fe43ad2b22bc696a21df
Jin-suyeon <wlstndus99@gmail.com>
2021-12-01 22:48:30 +0900

    메뉴리뉴얼

--- Programmers/level2/메뉴리뉴얼.js
+++ Programmers/level2/메뉴리뉴얼.js
@@ -1,23 +1,30 @@
 function solution(orders, course) {
-  orders = orders.map((el) => el.split(""));
+  // orders를 배열 형태로 만들어 줌
+  orders = orders.map((el) => el.split("").sort());
+
   const orderList = [];
   const numList = {};
   let result = [];
 
-  const combination = (arr, bucket, n) => {
-    if (arr.length < n) {
-      return;
+  //for문으로 재귀 돌려주기
+  for (let i = 0; i < orders.length; i++) {
+    for (let j = 0; j < course.length; j++) {
+      combination(orders[i], [], course[j]);
     }
+  }
+
+  // 조합 재귀 사용
+  function combination(arr, bucket, n) {
     if (n === 0) {
-      let order = bucket.sort().join("");
+      let order = bucket.join("");
       let idx = orderList.indexOf(order);
+
       if (idx === -1) {
         orderList.push(order);
         numList[order] = 1;
       } else {
         numList[order]++;
       }
-
       return;
     }
 
@@ -27,19 +34,19 @@ function solution(orders, course) {
 
       combination(sliceArr.slice(i + 1), bucket.concat(choice), n - 1);
     }
-  };
-
-  for (let i = 0; i < orders.length; i++) {
-    for (let j = 0; j < course.length; j++) {
-      combination(orders[i], [], course[j]);
-    }
   }
 
+  // 숫자가 주문조합과 같이 들어갈 수 있게 만들어줌
   let numsArr = orderList.map((el) => [el, numList[el]]);
 
+  // for문을 돌려서 course의 길이가 같은 것만 뽑아내어
+  // 그 중 가장 큰 수를 구한다
+  // 그 큰수와 조합이 같고 가장 큰 수가 2이상이면
+  // result에 push
   for (let i = 0; i < course.length; i++) {
     let filter = numsArr.filter((el) => el[0].length === course[i]);
     let maxNum = Math.max(...filter.map((el) => el[1]));
+
     for (let j = 0; j < filter.length; j++) {
       if (filter[j][1] === maxNum && maxNum >= 2) {
         result.push(filter[j][0]);
@@ -47,6 +54,7 @@ function solution(orders, course) {
     }
   }
 
+  // 오름차순으로 리턴
   return result.sort();
 }
 
