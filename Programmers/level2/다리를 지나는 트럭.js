d81874699530935ce7f67f6f1f05934e196e2b21
Jin-suyeon <wlstndus99@gmail.com>
2021-12-20 15:53:50 +0900

    다리를 지나는 트럭

---
+++ Programmers/level2/다리를 지나는 트럭.js
@@ -0,0 +1,29 @@
+function solution(bridge_length, weight, truck_weights) {
+  let bridge = new Array(bridge_length).fill(0);
+  let cnt = 0;
+
+  while (truck_weights.length > 0) {
+    let sum = bridge.reduce((a, b) => a + b, 0);
+
+    if (sum + truck_weights[0] <= weight) {
+      bridge.push(truck_weights.shift());
+      bridge.shift();
+      cnt++;
+    } else {
+      bridge.shift();
+      sum = bridge.reduce((a, b) => a + b, 0);
+
+      if (sum + truck_weights[0] <= weight) {
+        bridge.push(truck_weights.shift());
+        cnt++;
+      } else {
+        bridge.push(0);
+        cnt++;
+      }
+    }
+  }
+
+  return cnt + bridge_length;
+}
+
+console.log(solution(2, 10, [7, 4, 5, 6])); // 	8
