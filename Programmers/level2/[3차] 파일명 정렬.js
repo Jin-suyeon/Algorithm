9d2eaceb51231be5b9717391705d0a79f7979cc5
Jin-suyeon <wlstndus99@gmail.com>
2022-04-05 13:49:19 +0900

    [3차] 파일명 정렬

--- Programmers/level2/[3차] 파일명 정렬.js
+++ Programmers/level2/[3차] 파일명 정렬.js
@@ -1,62 +1,52 @@
 function solution(files) {
-  let divide = [];
-  let result = [];
+  // for문을 돌며 head, number, tail로 구분하고
+  // head가 모두 같은 경우에는 number로 구별하고
+  // 그게 아닌 경우에는 head의 순서에 맞춰 정렬한다
+  let heads = new Array(files.length).fill("");
+  let numbers = new Array(files.length).fill("");
+  let tails = new Array(files.length).fill("");
+  let filesCopy = [];
 
   for (let i = 0; i < files.length; i++) {
+    let file = files[i].split("");
     let num = /[0-9]/;
-    let numCheck = "";
-    for (let j = 0; j < files[i].split("").length; j++) {
-      if (num.test(files[i][j])) {
-        numCheck += files[i][j];
 
-        if (!num.test(files[i][j + 1])) {
-          break;
-        }
+    for (let j = 0; j < file.length; j++) {
+      if (num.test(file[j]) && !num.test(file[j + 1])) {
+        numbers[i] += file[j];
+        tails[i] = files[i].slice(j + 1);
+        break;
+      }
+      if (num.test(file[j]) && num.test(file[j + 1])) {
+        numbers[i] += file[j];
+      } else {
+        heads[i] += file[j];
       }
     }
-    divide.push({});
-    divide[i].num = numCheck;
-    let [head, tail] = files[i].split(numCheck);
-    divide[i].head = head;
-    divide[i].tail = tail;
   }
 
-  divide.sort((a, b) => {
-    let headA = a.head.toUpperCase();
-    let headB = b.head.toUpperCase();
-
-    if (headA < headB) {
-      return -1;
-    }
-    if (headA > headB) {
-      return 1;
-    }
-
-    return 0;
-  });
-
-  divide.sort((a, b) => {
-    let numA = Number(a.num);
-    let numB = Number(b.num);
-    let headA = a.head.toUpperCase();
-    let headB = b.head.toUpperCase();
-
-    if (headA === headB) {
-      if (numA < numB) {
-        return -1;
-      }
-      if (numA > numB) {
-        return 1;
-      }
-    }
-
-    return 0;
-  });
-
-  for (let i = 0; i < divide.length; i++) {
-    let { num, head, tail } = divide[i];
-    result.push(head + num + tail);
+  for (let i = 0; i < heads.length; i++) {
+    filesCopy.push([heads[i], numbers[i], tails[i]]);
   }
 
-  return result;
+  // 첫번째 head를 정렬하고, head에서 같은 것들이 있으면 number로 정렬한다
+  // head와 number가 모두 동일하다면 그 자리를 유지한다
+  filesCopy.sort((a, b) => {
+    if (a[0].toUpperCase() < b[0].toUpperCase()) return -1;
+    if (a[0].toUpperCase() > b[0].toUpperCase()) return 1;
+    if (a[0].toUpperCase() === b[0].toUpperCase()) return 0;
+  });
+  filesCopy.sort((a, b) => {
+    if (a[0].toUpperCase() === b[0].toUpperCase()) {
+      if (a[0] + Number(a[1]) !== b[0] + Number(b[1])) {
+        return Number(a[1]) - Number(b[1]);
+      }
+    }
+  });
+
+  for (let i = 0; i < filesCopy.length; i++) {
+    filesCopy[i] = filesCopy[i].join("");
+  }
+
+  return filesCopy;
 }
